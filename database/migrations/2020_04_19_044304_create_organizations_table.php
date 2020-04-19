7fcd14dc9de43f127b59f1ddd1fc355875bbfa2c
Paul Barham <paulb@pdblaptop.local>
2020-04-18 23:45:55 -0500

    Crated organizations table for an example

---
+++ database/migrations/2020_04_19_044304_create_organizations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60)->unique();
+            $table->string('alias', 16)->nullable()->default('');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+            $table->integer('created_by')->default(0);
+            $table->integer('modified_by')->default(0);
+            $table->integer('purged_by')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
